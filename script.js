aea2b21b27b14face4066728934cea8274ff0d56
Maiza Volpato dos Santos <maiza.volpato@gmail.com>
2025-08-01 15:09:37 +0100

    Create base html and css docs

---
+++ script.js
@@ -0,0 +1,4 @@
+let page = document.querySelector(".page");
+let profileName = page.querySelector(".profile__name");
+let profiledatail = page.querySelector(".profile__datail");
+let editButtonProfile = page.querySelector(".buton__profile__edit");
